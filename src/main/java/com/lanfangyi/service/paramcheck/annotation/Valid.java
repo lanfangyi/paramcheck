a92563b56cba24fca1bc137977c3623b1626de40
lanfangyi <1069344911@qq.com>
2019-08-16 14:32:25 +0800

    删除一些没必要的属性

--- src/main/java/com/lanfangyi/service/paramcheck/annotation/Valid.java
+++ src/main/java/com/lanfangyi/service/paramcheck/annotation/Valid.java
@@ -12,18 +12,29 @@ import java.lang.annotation.*;
 @Documented
 public @interface Valid {
 
-    String message() default "";
-
+    /**
+     * 当出错的时候，返回值所在类
+     */
     Class msgClass() default Valid.class;
 
+    /**
+     * 当校验不通过的时候，接口的返回值，这个值要定义在某个产量文件中。定义之后，程序会去读取并识别类型是否与接口
+     */
     String msgClassStaticField() default "";
 
-    boolean setErrMsg() default false;
-
+    /**
+     * 当校验出现错误的时候，是否记日志。
+     */
     boolean addErrLog() default false;
 
+    /**
+     * 如果addErrLog 为true， 则记日志，此变量是日志的级别
+     */
     ErrorLevelEnum logLevel() default ErrorLevelEnum.INFO;
 
+    /**
+     * 日志信息
+     */
     String logMsg() default "";
 
 }
