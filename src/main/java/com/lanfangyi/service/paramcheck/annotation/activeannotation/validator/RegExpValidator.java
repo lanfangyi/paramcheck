e48dcdeecfb2ed2a9ee769a74c50c236c5f16347
lanfangyi <1069344911@qq.com>
2019-08-29 16:40:42 +0800

    增加null的判断

--- src/main/java/com/lanfangyi/service/paramcheck/annotation/activeannotation/validator/RegExpValidator.java
+++ src/main/java/com/lanfangyi/service/paramcheck/annotation/activeannotation/validator/RegExpValidator.java
@@ -10,10 +10,13 @@ import java.util.regex.Pattern;
 public class RegExpValidator implements Validateable {
     @Override
     public ValidateResult valid(Annotation annotation, Object param, String paramName) {
+        if (null == param) {
+            return ValidateResult.nullValiddateResult(paramName);
+        }
         ValidateResult validateResult = null;
         String s = String.valueOf(param);
         RegExp regExp = (RegExp) annotation;
-        if (param == null || !Pattern.matches(regExp.value(), s)) {
+        if (!Pattern.matches(regExp.value(), s)) {
             validateResult = new ValidateResult();
             validateResult.setCode(405);
             validateResult.setValidMsg(paramName + "参数不符合正则");
